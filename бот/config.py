d71b939293dbfdda28f14f26fa444dd8c2a9a4ba
Lepsps <122830654+Lepsps@users.noreply.github.com>
2023-01-20 01:37:47 +0300

    Add files via upload

---
+++ бот/config.py
@@ -0,0 +1,13 @@
+#-*-coding utf-8-*-
+
+# Это конфигурационный файл.
+# Если вы не понимаете, что с этим делать,
+
+global botkey
+global qiwi_token
+
+botkey = '5711574089:AAFCZguhaH_Y7k9KszQcZyEhVePCWwASncY'
+owners_id = ['896130275']
+qiwi_token = "12"
+
+logToFile = True
